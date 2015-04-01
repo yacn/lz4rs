1c9d7dceb90481fbe174f22b15c6e0666e69c378
yacn <admin@yacn.me>
2015-04-01 19:12:21 -0400

    add basic decompression function

--- src/liblz4/frame.rs
+++ src/liblz4/frame.rs
@@ -1,14 +1,17 @@
+//! Bindings to functions contained within lz4frame.h
+
 #![allow(unstable)]
 #![allow(non_snake_case)]
 
 extern crate libc;
 
-use libc::{c_uint, c_int, size_t, c_char};
+use libc::{c_uint, c_int, size_t, c_char, c_void};
 
 use std::ffi::CString;
 
 pub type LZ4F_errorCode_t = size_t;
 
+#[derive(Show)]
 #[repr(C)]
 pub enum blockSizeID_t {
 	LZ4F_default = 0,
@@ -18,24 +21,28 @@ pub enum blockSizeID_t {
 	max4MB = 7,
 }
 
+#[derive(Show)]
 #[repr(C)]
 pub enum blockMode_t {
 	blockLinked = 0,
 	blockIndependent,
 }
 
+#[derive(Show)]
 #[repr(C)]
 pub enum contentChecksum_t {
 	noContentChecksum = 0,
 	contentChecksumEnabled,
 }
 
+#[derive(Show)]
 #[repr(C)]
 pub enum frameType_t {
 	LZ4F_frame = 0,
 	skippableFrame,
 }
 
+#[derive(Show)]
 #[repr(C)]
 pub struct LZ4F_frameInfo_t {
 	pub blockSizeID: blockSizeID_t,
@@ -54,6 +61,22 @@ pub struct LZ4F_preferences_t {
 	pub reserved: [c_uint; 4],
 }
 
+pub const LZ4F_VERSION: c_uint = 100;
+
+pub type LZ4F_decompressionContext_t = *mut c_void;
+
+#[repr(C)]
+pub struct LZ4F_decompressOptions_t {
+	stableDst: c_uint,
+	reserved: [c_uint; 3],
+}
+
+// typedef struct {
+//  unsigned stableSrc;    /* 1 == src content will remain available on future calls to LZ4F_compress(); avoid saving src content within tmp buffer as future dictionary */
+//  unsigned reserved[3];
+// } LZ4F_compressOptions_t;
+
+// #define LZ4F_VERSION 100
 extern {
 
 	// lz4frame.h
@@ -68,6 +91,42 @@ extern {
 	pub fn LZ4F_compressFrameBound(srcSize: size_t, prefsPtr: *const LZ4F_preferences_t) -> size_t;
 
 	//size_t LZ4F_compressFrame(void* dstBuffer, size_t dstMaxSize, const void* srcBuffer, size_t srcSize, const LZ4F_preferences_t* preferencesPtr);
-	pub fn LZ4F_compressFrame(dstBuffer: *mut u8, dstMaxSize: size_t, srcBuffer: *const u8, srcSize: size_t, prefsPtr: *const LZ4F_preferences_t) -> size_t;
+	pub fn LZ4F_compressFrame(dstBuffer: *mut c_void, dstMaxSize: size_t, srcBuffer: *const c_void, srcSize: size_t, prefsPtr: *const LZ4F_preferences_t) -> size_t;
+
+	//LZ4F_errorCode_t LZ4F_createCompressionContext(LZ4F_compressionContext_t* cctxPtr, unsigned version);
+	fn LZ4F_createCompressionContext(cctxPtr: *mut c_void, version: c_uint) -> LZ4F_errorCode_t;
+
+	// TODO
+	//LZ4F_errorCode_t LZ4F_freeCompressionContext(LZ4F_compressionContext_t cctx);
+	//fn LZ4F_freeCompressionContext(cctx: ...) -> LZ4F_errorCode_t
+
+	//size_t LZ4F_compressBegin(LZ4F_compressionContext_t cctx, void* dstBuffer, size_t dstMaxSize, const LZ4F_preferences_t* prefsPtr);
+	//fn LZ4F_compressBegin(cctx: ..., dstBuffer: *mut u8, dstMaxSize: size_t, prefsPtr: ...)
+
+	//fn LZ4F_decompress(dctx: *mut c_void, dstBuffer: *mut c_void, 
+
+	pub fn LZ4F_decompress(dctx: *mut c_void, dstBuffer: *mut c_void, dstSizePtr: *mut size_t, srcBuffer: *const c_void, srcSizePtr: *mut size_t, dOptPtr: *const LZ4F_decompressOptions_t) -> size_t;
+
+	//size_t LZ4F_getFrameInfo(LZ4F_decompressionContext_t dctx,
+                         //LZ4F_frameInfo_t* frameInfoPtr,
+                         //const void* srcBuffer, size_t* srcSizePtr);
+
+	pub fn LZ4F_getFrameInfo(dctx: *mut c_void, frameInfoPtr: *mut LZ4F_frameInfo_t, srcBuffer: *const c_void, srcSizePtr: *mut size_t) -> size_t;
+
+	pub fn LZ4F_createDecompressionContext(dctxPtr: *mut *mut c_void, version: c_uint) -> LZ4F_errorCode_t;
+	pub fn LZ4F_freeDecompressionContext(dctx: *mut c_void) -> LZ4F_errorCode_t;
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ fn main() {
 	let suffix = ".lz4";
 
 	for arg in os::args().tail().iter() {
-		lz4rs::compress(&Path::new(arg), &Path::new(&(arg.to_string() + suffix))).unwrap();
+		//lz4rs::compress(&Path::new(arg), &Path::new(&(arg.to_string() + suffix))).unwrap();
+		lz4rs::decompress(&Path::new(arg), &Path::new(&arg[0..arg.len()-suffix.len()])).unwrap();
 	}
 }
